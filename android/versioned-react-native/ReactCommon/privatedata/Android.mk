8b0581d01a27c2d1d5ce51369504f3a48c0c6aa1
Jesse Ruder <jesse@expo.io>
2017-11-15 00:50:35 +0000

    Version SDK 23
    fbshipit-source-id: d18455f

--- android/versioned-react-native/ReactCommon/privatedata/Android.mk
+++ android/versioned-react-native/ReactCommon/privatedata/Android.mk
@@ -2,12 +2,10 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := jschelpers_abi23_0_0
+LOCAL_MODULE := privatedata_abi23_0_0
 
 LOCAL_SRC_FILES := \
-  Unicode.cpp \
-  JSCHelpers.cpp \
-  Value.cpp \
+  PrivateDataBase.cpp \
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/..
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
@@ -20,12 +18,5 @@ CXX11_FLAGS := -std=c++11
 LOCAL_CFLAGS += $(CXX11_FLAGS)
 LOCAL_EXPORT_CPPFLAGS := $(CXX11_FLAGS)
 
-LOCAL_SHARED_LIBRARIES := libfolly_json_abi23_0_0 libjsc libglog_abi23_0_0
-
-include $(BUILD_STATIC_LIBRARY)
-
-$(call import-module,folly)
-$(call import-module,jsc)
-$(call import-module,glog)
-$(call import-module,privatedata)
+include $(BUILD_SHARED_LIBRARY)
 
